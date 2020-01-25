7f95365d6d74e4b021ebca051117d04832a5f2f2
Robert Kaczmarek <rkaczmarek@prograils.com>
2020-01-25 16:59:24 +0100

    Revert "Remove unused images, minor tweaks to views, add validation to donation"
    This reverts commit 124c2575e3f9183f51c57e2aa215b079efe2d671.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,8 +2,4 @@ module ApplicationHelper
   def current_path
     request.path
   end
-
-  def active_tab(path)
-    'active' if current_path == path
-  end
 end
--- app/models/donation.rb
+++ app/models/donation.rb
@@ -5,15 +5,4 @@ class Donation < ApplicationRecord
   scope :attended, -> { where attended: true }
   scope :latest, -> { order(created_at: :desc) }
   scope :in_year, ->(year) { where('extract(year from donations.created_at) = ?', year) }
-
-  validate :donation_date, on: :create
-
-  private
-
-  def donation_date
-    next_donation_after = donor.donations.last.date + 8.weeks
-    return true if next_donation_after < date
-
-    errors.add(:base, "You can donate after: #{next_donation_after}")
-  end
 end
